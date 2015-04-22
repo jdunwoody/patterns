c9d31f4cd0892f6409616b7fbe18404494993bc1
James Dunwoody <jdunwoody@odecee.com.au>
2015-04-22 11:47:47 +1000

    Created Factory

---
+++ Patterns/Classes/Patterns/Factory.h
@@ -0,0 +1,13 @@
+//
+//  Factory.h
+//  Patterns
+//
+//  Created by James Dunwoody on 22/04/2015.
+//  Copyright (c) 2015 JamesDunwoody. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Factory : NSObject
+
+@end
---
+++ Patterns/Classes/Patterns/Factory.m
@@ -0,0 +1,13 @@
+//
+//  Factory.m
+//  Patterns
+//
+//  Created by James Dunwoody on 22/04/2015.
+//  Copyright (c) 2015 JamesDunwoody. All rights reserved.
+//
+
+#import "Factory.h"
+
+@implementation Factory
+
+@end
