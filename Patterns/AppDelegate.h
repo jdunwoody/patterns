d29eaa876eadc859058c6fd0f75de1e8103aba6a
James Dunwoody <jdunwoody@odecee.com.au>
2015-04-22 10:20:17 +1000

    Created XCode project

---
+++ Patterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns
+//
+//  Created by James Dunwoody on 22/04/2015.
+//  Copyright (c) 2015 JamesDunwoody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Patterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns
+//
+//  Created by James Dunwoody on 22/04/2015.
+//  Copyright (c) 2015 JamesDunwoody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
